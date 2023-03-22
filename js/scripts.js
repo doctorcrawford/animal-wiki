a330918a134f72e1c7d0f6c791d58a910f902a79
Kyle Crawford <kcraw217@gmail.com>
2023-03-22 12:04:51 -0700

    search functionality for our animals and hides other info
    Co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,24 @@
-const input = document.querySelector("#animal").value;
+function hide() {
+  document.querySelector("#goats").setAttribute("class", "hidden")
+  document.querySelector("#ducks").setAttribute("class", "hidden")
+  document.querySelector("#platypus").setAttribute("class", "hidden")
+  document.querySelector("#any-other-animal").setAttribute("class", "hidden")
+}
+
 const form = document.querySelector("form")
+form.onsubmit = function(e) {
+  e.preventDefault();
+
+  const input = document.querySelector("#animal").value;
+  hide()
+
+  if (input === "platypus") {
+    document.querySelector("#platypus").removeAttribute("class")
+  } else if (input === "ducks") {
+    document.querySelector("#ducks").removeAttribute("class")
+  } else if (input === "goats") {
+    document.querySelector("#goats").removeAttribute("class")
+  } else { 
+    document.querySelector("#any-other-animal").removeAttribute("class") }
+    document.querySelector("span#other-animal").innerText = input
+}
